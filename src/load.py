9a71d8888ad2dd1210dff9861d49928bff954148
auto-user <auto@example.com>
2025-11-17 18:01:33 +0000

    Publish processed data

--- src/load.py
+++ src/load.py
@@ -31,4 +31,4 @@ def load_data(repo_url):
 
 if __name__ == "__main__":
     # USER CAN CHANGE THIS:
-    load_data("https://github.com/jessicabpa9-hub/test2.git")
+    load_data("https://github.com/jessicabpa9-hub/test3.git")
